e33219103a5853e33f11211a74bbc7070727e472
Laszlo Varga <varga.laszlo.drz@gmail.com>
2024-11-07 09:11:29 +0100

    add
    radio buttons, but still don't work

--- work/script/show.rawExamin.js
+++ work/script/show.rawExamin.js
@@ -5,26 +5,48 @@ const examin = {
     output: [],
     labels: [],
     data: [],
+    chartInstance: null, // A meglévő diagram tárolására
+    currentChartType: 'line', // Alapértelmezett diagram típus
+
     show(event, dataID) {
         this.output.length = 0;
         this.labels.length = 0;
         this.data.length = 0;
 
-        // search the index of array
-        tempIndex = pluto.inputData.ID.indexOf(dataID);
+        // Keresés az array indexében
+        const tempIndex = pluto.inputData.ID.indexOf(dataID);
 
-        // header
-        this.output.push('<h4 id="play-descriptive-statistic"><u>' + (tempIndex + 1) + ". adatsor</u> grafikonja és leíró statisztikája</h4>");
+        // Fejléc
+        this.output.push('<h4 id="play-descriptive-statistic">A <u>kijelölt</u> adatsorok megjeleítése</h4>');
         this.output.push('<div class="container-fluid">');
         this.output.push('<div class="row">');
 
-        // add canvas to DOM
+        // Canvas hozzáadása a DOM-hoz
         this.output.push('<div class="col-mg-9 col-lg-9" id="play-chart">');
         this.output.push('<canvas id="showExaminChart" style="width: 100%; height: 500px;"></canvas>');
         this.output.push('</div>');
+        this.output.push('<div class="col-mg-3 col-lg-3">');
 
-        // refresh DOM
+        // Rádiógombok hozzáadása a diagram típusának kiválasztásához
+        this.output.push(`
+            <div id="chartTypeSelector">
+                <label><input type="radio" name="chartType" value="line" checked> Vonal</label>
+                <label><input type="radio" name="chartType" value="bar"> Oszlop</label>
+                <label><input type="radio" name="chartType" value="radar"> Radar</label>
+            </div>
+        `);
+
+        // DOM frissítése
         document.getElementById("floatbox-content").innerHTML = this.output.join("");
+
+        /*
+        document.querySelectorAll('input[name="chartType"]').forEach((radio) => {
+            radio.addEventListener('change', (event) => {
+                examin.currentChartType = event.target.value;
+                examin.updateChart(); // Diagram frissítése a kiválasztott típus alapján
+                console.log(examin.currentChartType);
+            });
+        });*/
 
         // Ellenőrizzük, hogy valóban létezik-e a canvas
         const canvas = document.getElementById('showExaminChart');
@@ -33,36 +55,40 @@ const examin = {
             return;
         }
 
-        // data for chart
-        // DEV: ellenőrizni!
+        // Adatok a diagramhoz
         pluto.inputData.all[tempIndex].forEach((item, index) => {
             this.labels.push(index);
-            //this.data.push(item);
         });
 
-        // end divs
-        this.output.push('</div></div>');
+        // Diagram inicializálása
+        this.addRadioEventListeners(); // Eseményfigyelők hozzáadása a rádiógombokhoz
 
         return this.output.join("");
     },
-    chartJS(multiDataY) {
 
+    chartJS(multiDataY) {
         const borderColors = ['#922b21', '#76448a', '#1f618d', '#148f77', '#1e8449', '#b7950b','#af601a','#717d7e', '#283747', '#000000'];
 
         const datasets = multiDataY.map((dataset, index) => ({
             label: `Adatsor ${index + 1}`,
             data: dataset,
-            //borderColor: `rgba(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, 1)`,
             borderColor: borderColors[index],
             fill: false,
             yAxisID: `y-axis-${index + 1}`,
         }));
 
         const ctx = document.getElementById('showExaminChart').getContext('2d');
-        const myChart = new Chart(ctx, {
-            type: 'line',
+
+        // Meglévő diagram törlése, ha van
+        if (this.chartInstance) {
+            this.chartInstance.destroy();
+        }
+
+        // Új diagram létrehozása a kiválasztott típus alapján
+        this.chartInstance = new Chart(ctx, {
+            type: this.currentChartType,
             data: {
-                labels: this.labels, // X ax
+                labels: this.labels,
                 datasets: datasets
             },
             options: {
@@ -80,14 +106,14 @@ const examin = {
                             scaleLabel: {
                                 display: true,
                                 labelString: `Adatsor ${index + 1} tengely`,
-                                fontColor: color, // Tengely címke színe
+                                fontColor: color,
                             },
                             ticks: {
                                 beginAtZero: true,
-                                fontColor: color, // Címkék színe a tengely mentén
+                                fontColor: color,
                             },
                             gridLines: {
-                                display: index === 0, // Csak az első tengelyhez rácsvonal
+                                display: index === 0,
                             },
                         };
                     }),
@@ -96,24 +122,40 @@ const examin = {
                             display: true,
                             labelString: 'X tengely',
                         }
-                    }],
-                    x: {
-                        beginAtZero: true
-                    },
-                    y: {
-                        beginAtZero: true
-                    },
-
-                },
-
+                    }]
+                }
             }
         });
+    },
+
+    // Rádiógomb eseményfigyelők hozzáadása
+    addRadioEventListeners() {
+        const radios = document.querySelectorAll('input[name="chartType"]');
+        console.log("Rádiógombok:", radios);
+    
+        radios.forEach((radio) => {
+            radio.addEventListener('click', (event) => {
+                console.log("Kiválasztott diagram típus:", event.target.value);
+                //this.currentChartType = event.target.value;
+                //this.updateChart(); 
+            });
+        });      
+    },
+
+
+    // Diagram frissítése
+    updateChart() {
+        const multiDataY = []; // Az adatsorok betöltése
+        selectedRectangles.forEach((rect, index) => {
+            const dataID = rect.getAttribute("data-id");
+            multiDataY.push(pluto.inputData.all[pluto.inputData.ID.indexOf(dataID)]);
+        });
+        this.chartJS(multiDataY);
     }
-}
+};
 
-
-
-// event
+// events
+// Esemény a "raw-examin" gombra
 buttonRawExamin.addEventListener('click', (event) => {
     let dataID = [];
     let multiDataY = [];
@@ -125,20 +167,18 @@ buttonRawExamin.addEventListener('click', (event) => {
             alert('A demó verzió 5 kijelölt adatsorral képes dolgozni.');
             return;
         }
-
     });
 
-    console.log(multiDataY[0][0]);
-
-    // data for floatbox
+    // Adatok betöltése a lebegő ablakhoz
     const data = examin.show(event, dataID[0]);
 
-    // open floatbox
+    // Lebegő ablak megnyitása
     floatBox.open(event, data, sizer.width - 100);
 
-    // add JS
+    // Diagram hozzáadása
     examin.chartJS(multiDataY);
 
+});
 
 
-})
+
